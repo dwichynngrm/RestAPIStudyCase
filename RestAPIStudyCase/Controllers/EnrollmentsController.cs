bd2810963f3717a19597f896939ec33079fd22b3
Dwi Cahyaningrum <dwicahyaningrum23@gmail.com>
2022-01-10 22:25:09 +0700

    membuat controller

---
+++ RestAPIStudyCase/Controllers/EnrollmentsController.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestAPIStudyCase.Data;
+using RestAPIStudyCase.Dtos;
+using RestAPIStudyCase.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestAPIStudyCase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+        private IEnrollment _enrollment;
+        private IMapper _mapper;
+        public EnrollmentsController(IEnrollment enrollment, IMapper mapper)
+        {
+            _enrollment = enrollment ?? throw new ArgumentNullException(nameof(enrollment));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        // GET: api/<EnrollmentsController>
+        [HttpGet]
+        public async Task<IEnumerable<Enrollment>> Get()
+        {
+            var results = await _enrollment.GetAll();
+            return results;
+        }
+
+        // GET api/<EnrollmentsController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Enrollment>> GetById(int id)
+        {
+            var result = await _enrollment.GetById(id.ToString());
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        // POST api/<EnrollmentsController>
+        [HttpPost]
+        public async Task<ActionResult<Enrollment>> Post([FromBody] EnrollmentForCreateDto enrollmentforCreateDto)
+        {
+            try
+            {
+                var enrollment = _mapper.Map<Models.Enrollment>(enrollmentforCreateDto);
+                var result = await _enrollment.Insert(enrollment);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT api/<EnrollmentsController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Enrollment>> Put(int id, [FromBody] EnrollmentForCreateDto enrollmentToCreateDto)
+        {
+            try
+            {
+                var course = _mapper.Map<Models.Enrollment>(enrollmentToCreateDto);
+                var result = await _enrollment.Update(id.ToString(), course);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/<EnrollmentsController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _enrollment.Delete(id.ToString());
+                return Ok($"delete data id {id} berhasil");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ RestAPIStudyCase/Controllers/UsersController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestAPIStudyCase.Data;
+using RestAPIStudyCase.Dtos;
+using RestAPIStudyCase.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestAPIStudyCase.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUser _user;
+        public UsersController(IUser user)
+        {
+            _user = user;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<ActionResult> Registration(CreateUserDto user)
+        {
+            try
+            {
+                await _user.Registration(user);
+                return Ok($"Registrasi user {user.Username} berhasil");
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<UserDto>> GetAll()
+        {
+            return Ok(_user.GetAllUser());
+        }
+
+        [HttpPost("Role")]
+        public async Task<ActionResult> AddRole(CreateRoleDto roleDto)
+        {
+            try
+            {
+                await _user.AddRole(roleDto.RoleName);
+                return Ok($"Tambah role {roleDto.RoleName} berhasil");
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("Role")]
+        public ActionResult<IEnumerable<CreateRoleDto>> GetAllRole()
+        {
+            return Ok(_user.GetRoles());
+        }
+
+        [HttpPost("UserInRole")]
+        public async Task<ActionResult> AddUserToRole(string username, string role)
+        {
+            try
+            {
+                await _user.AddUserToRole(username, role);
+                return Ok($"Berhasil menambahkan user {username} ke role {role}");
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("RolesByUser/{username}")]
+        public async Task<ActionResult<List<string>>> GetRolesByUser(string username)
+        {
+            var results = await _user.GetRolesFromUser(username);
+            return Ok(results);
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost("Authentication")]
+        public async Task<ActionResult<User>> Authentication(CreateUserDto createUserDto)
+        {
+            try
+            {
+                var user = await _user.Authenticate(createUserDto.Username, createUserDto.Password);
+                if (user == null)
+                    return BadRequest("username/password tidak tepat");
+                return Ok(user);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
