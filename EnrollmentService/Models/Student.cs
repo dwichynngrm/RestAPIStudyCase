a96973d29eade7a1137d549a457e1a7435bc7b67
Dwi Cahyaningrum <dwicahyaningrum23@gmail.com>
2022-01-11 23:12:34 +0700

    add models and migration

---
+++ EnrollmentService/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EnrollmentService.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
