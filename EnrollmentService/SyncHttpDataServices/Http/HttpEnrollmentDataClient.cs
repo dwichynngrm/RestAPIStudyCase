9b540e33af26a0580f8ff364357e6671264a3e58
Dwi Cahyaningrum <dwicahyaningrum23@gmail.com>
2022-01-11 23:15:54 +0700

    add app settings and sync http

---
+++ EnrollmentService/SyncHttpDataServices/Http/HttpEnrollmentDataClient.cs
@@ -0,0 +1,43 @@
+﻿using EnrollmentService.Dtos;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace EnrollmentService.SyncHttpDataServices.Http
+{
+    public class HttpEnrollmentDataClient : IEnrollmentDataClient
+    {
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _httpClient;
+
+        public HttpEnrollmentDataClient()
+        {
+        }
+
+        public HttpEnrollmentDataClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _httpClient = httpClient;
+        }
+        public async Task CreateEnrollmentFromPaymentService(EnrollmentForCreateDto enrollment)
+        {
+            var httpContent = new StringContent(
+               JsonSerializer.Serialize(enrollment),
+               Encoding.UTF8, "application/json"
+           );
+
+            var response = await _httpClient.PostAsync(_configuration["PaymentService"], httpContent);
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("--> Sync Post to Command Service Success -->");
+            }
+            else
+            {
+                Console.WriteLine("--> Sync Post to Command Service Failed -->");
+            }
+        }
+    }
+}
---
+++ EnrollmentService/SyncHttpDataServices/Http/IEnrollmentDataClient.cs
@@ -0,0 +1,11 @@
+﻿using EnrollmentService.Dtos;
+using System.Threading.Tasks;
+using EnrollmentService.Models;
+
+namespace EnrollmentService.SyncHttpDataServices.Http
+{
+    public interface IEnrollmentDataClient
+    {
+        Task CreateEnrollmentFromPaymentService(EnrollmentForCreateDto enrollment);
+    }
+}
